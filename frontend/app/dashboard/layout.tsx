e2eaa0dc34fe8a1b3834cda109df16b525219ba1
Victor Ajibade <ajibadevic@gmail.com>
2024-08-22 15:56:55 +0100

    feat: add logout button

--- frontend/app/dashboard/layout.tsx
+++ frontend/app/dashboard/layout.tsx
@@ -1,7 +1,8 @@
 "use client";
 import SideNav from "@/components/nav/side-nav";
 import { useMediaQuery } from "@/hooks/useMediaQuery";
-import { MenuIcon } from "lucide-react";
+import { LogOutIcon, MenuIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 export default function DashboardLayout({
@@ -11,6 +12,7 @@ export default function DashboardLayout({
 }) {
   const [hideSideNav, setHideSideNav] = useState(true);
   const isMobileScreen = useMediaQuery("(max-width: 1023px)");
+  const router = useRouter();
 
   return (
     <section className="min-h-screen bg-woodsmoke text-white">
@@ -21,13 +23,20 @@ export default function DashboardLayout({
       )}
 
       <div className="p-8 lg:ml-[105px] xl:ml-[210px]">
-        <nav className="fixed left-0 right-0 top-0 z-10 flex h-14 w-full items-center border-b border-b-mineshaft bg-shark px-4 lg:hidden">
+        <nav className="fixed left-0 right-0 top-0 z-10 flex h-14 w-full items-center justify-between border-b border-b-mineshaft bg-shark px-4 lg:hidden">
           <button
             className="flex items-center justify-center"
             onClick={() => setHideSideNav(!hideSideNav)}
           >
             <MenuIcon className="text-white" />
           </button>
+
+          <div
+            className="flex items-center justify-center"
+            onClick={() => router.push("/login")}
+          >
+            <LogOutIcon />
+          </div>
         </nav>
 
         <div className="mt-14 lg:mt-0">{children}</div>
