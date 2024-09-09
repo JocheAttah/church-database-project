76e49d541bc59ada6b832e88cdf7f670c80332ae
Victor Ajibade <ajibadevic@gmail.com>
2024-09-09 09:31:04 +0100

    fix: dialog overflow and table no results pointer event

--- frontend/components/tables/members/data-table.tsx
+++ frontend/components/tables/members/data-table.tsx
@@ -145,7 +145,7 @@ export function DataTable<TData, TValue>({
               <TableRow>
                 <TableCell
                   colSpan={columns.length}
-                  className="h-24 text-center"
+                  className="pointer-events-none h-24 text-center"
                 >
                   No results found.
                 </TableCell>
